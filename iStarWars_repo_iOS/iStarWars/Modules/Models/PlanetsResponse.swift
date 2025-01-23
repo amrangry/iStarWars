5bb907f048a2fbd4eb0e088eacda25ba23c2d99f
Amr Angry <2900952+amrangry@users.noreply.github.com>
2025-01-23 13:46:49 +0400

    model

---
+++ iStarWars_repo_iOS/iStarWars/Modules/Models/PlanetsResponse.swift
@@ -0,0 +1,18 @@
+//
+//  PlanetsResponse.swift
+//  iStarWars
+//
+//  Created by Amr Ahmed Elghadban on 23/01/2025.
+//
+
+import Foundation
+
+struct PlanetsResponse: Codable {
+    let results: [Planet]
+    let count: Int
+    let next: String?
+    let previous: String?
+    //    "count": 60,
+    //    "next": "https://swapi.dev/api/planets/?page=2",
+    //    "previous": null,
+}
