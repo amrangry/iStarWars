ceae47a0ebfa41891fbafc2aec2e2f2f7a290c54
Amr Angry <2900952+amrangry@users.noreply.github.com>
2025-01-23 13:45:46 +0400

    Errors added

---
+++ iStarWars_repo_iOS/iStarWars/Utilities/Errors/NetworkError.swift
@@ -0,0 +1,34 @@
+//
+//  NetworkError.swift
+//  iStarWars
+//
+//  Created by Amr Ahmed Elghadban on 23/01/2025.
+//
+
+import Foundation
+
+//NetworkError is an enum that conforms to the Error and LocalizedError protocols.
+enum NetworkError: Error, LocalizedError {
+    
+    //Represents network-related errors.
+    case networkError(description: String)
+    //Represents an invalid URL error.
+    case invalidURL
+    case noInternetConnection
+    case timeout
+    
+    // Provide user-friendly error messages
+    var errorDescription: String? {
+        switch self {
+            case .networkError(let description):
+                return "Network Error: \(description)"
+            case .invalidURL:
+                return "Invalid URL"
+            case .timeout:
+                return "Timeout"
+            case .noInternetConnection:
+                return "No Internet Connection"
+        }
+    }
+    
+}
