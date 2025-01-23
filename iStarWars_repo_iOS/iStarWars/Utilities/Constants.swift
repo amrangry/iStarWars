4436305b1aedca33efe5283104c8f1896a121c2d
Amr Angry <2900952+amrangry@users.noreply.github.com>
2025-01-23 13:46:16 +0400

    setup config

---
+++ iStarWars_repo_iOS/iStarWars/Utilities/Constants.swift
@@ -0,0 +1,24 @@
+//
+//  Constants.swift
+//  iStarWars
+//
+//  Created by Amr Ahmed Elghadban on 23/01/2025.
+//
+
+import Foundation
+
+// Application Constants
+struct Constants {
+    
+    // Configuration Network
+    static var currentEnvironment: Environment = .production
+    static var baseURL: String {
+        return currentEnvironment.baseURL
+    }
+    // Configuration URLCache
+    static let cacheExpiryInterval: TimeInterval = 3600 // 1 hour
+    static let cacheSizeMemory = 50 * 1024 * 1024 // 50 MB
+    static let cacheSizeDisk = 100 * 1024 * 1024 // 100 MB
+    static let cacheDiskPath = "iStarWarCache"
+    
+}
---
+++ iStarWars_repo_iOS/iStarWars/Utilities/Environment.swift
@@ -0,0 +1,27 @@
+//
+//  Environment.swift
+//  iStarWars
+//
+//  Created by Amr Ahmed Elghadban on 23/01/2025.
+//
+
+import Foundation
+
+enum Environment {
+    
+    case development
+    case staging
+    case production
+    
+    var baseURL: String {
+        switch self {
+            case .development:
+                return "https://dev.swapi.dev/api"
+            case .staging:
+                return "https://staging.swapi.dev/api"
+            case .production:
+                return "https://swapi.dev/api"
+        }
+    }
+    
+}
