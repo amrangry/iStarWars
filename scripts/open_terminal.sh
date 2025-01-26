c60571d17b9b4fa35044e95510b61209fdec0bb2
Amr Angry <2900952+amrangry@users.noreply.github.com>
2025-01-27 02:33:19 +0400

    update scripts

--- scripts/open_terminal.sh
+++ scripts/open_terminal.sh
@@ -5,3 +5,11 @@
 # Workspace Dir: $XcodeWorkspacePath
 
 open -a Terminal "$(dirname $XcodeProjectPath)" 
+
+## Pass Xcode environment variables to the script
+#XcodeProject="${PROJECT_NAME}.xcodeproj"
+#XcodeProjectPath="${PROJECT_DIR}/${PROJECT_NAME}.xcodeproj"
+#XcodeWorkspacePath="${PROJECT_DIR}/${PROJECT_NAME}.xcworkspace"
+#
+## Call the script
+#./open_terminal.sh
