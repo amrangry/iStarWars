0dee065738056916d31319c0870b74c60cc2db70
Amr Angry <2900952+amrangry@users.noreply.github.com>
2025-01-26 22:33:32 +0400

    rename variable to index

--- iStarWars/Modules/Repositories/PlanetRepository.swift
+++ iStarWars/Modules/Repositories/PlanetRepository.swift
@@ -38,7 +38,7 @@ class PlanetRepository: PlanetRepositoryProtocol {
                 // Assign identifiers based on the array index
                 let planets = response.results.enumerated().map { index, planet in
                     var planetWithIdentifier = planet
-                    planetWithIdentifier.identifier = "\(index + 1)" // Start from 1
+                    planetWithIdentifier.index = "\(index + 1)" // Start from 1
                     return planetWithIdentifier
                 }
                 
