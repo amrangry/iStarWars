babb605ee9327ddc72257751634735aedb7fdaa4
Amr Angry <2900952+amrangry@users.noreply.github.com>
2025-02-05 13:57:44 +0400

    Create PlanetRepositoryStorage.swift
    Init for Storage repo

---
+++ iStarWars/Modules/Repositories/PlanetRepositoryStorage.swift
@@ -0,0 +1,26 @@
+//
+//  PlanetRepositoryStorage.swift
+//  iStarWars
+//
+//  Created by Amr Ahmed Elghadban on 28/01/2025.
+//
+
+import Foundation
+import Combine
+
+class PlanetRepositoryStorage: PlanetRepositoryProtocol {
+    
+    func fetchPlanets() -> AnyPublisher<[Planet], any Error> {
+        //TODO - implementation
+        let planets = [Planet.mock(), Planet.mock()];
+        return Just(planets)
+            .setFailureType(to: Error.self) //to align with the return type (Publisher)
+            .eraseToAnyPublisher()
+    }
+    
+    func fetchPlanetDetails(id: String) async throws -> Planet {
+        //TODO - implementation
+        Planet.mock()
+    }
+    
+}
