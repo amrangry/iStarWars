59f40ffe4453db04ce42f8c3ae24c4784aee8082
Amr Angry <amr.elghadban@gmail.com>
2025-02-12 19:54:10 +0400

    Update AppCoordinator.swift

--- iStarWars/Coordinator/AppCoordinator.swift
+++ iStarWars/Coordinator/AppCoordinator.swift
@@ -12,7 +12,7 @@ import SwiftUI
 class AppCoordinator: ObservableObject {
 
     @Published var navigationPath = [NavigationPath]()
-
+    
     private let sceneConfigurator = SceneConfigurator.shared
 
     func navigate(to path: NavigationPath) {
